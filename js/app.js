2cd45cb0afb41499a0fb233fac05fcd5c5def6a1
Antahkarana98 <Antahkarana98@github.com>
2023-03-08 15:03:27 -0500

    agregando criptos al select

--- js/app.js
+++ js/app.js
@@ -1,13 +1,29 @@
-const criptomenedasSelect = document.querySelector('#criptomenedas');
+const criptomenedasSelect = document.querySelector('#criptomonedas');
+
+const obtenerCriptomonedas = criptomonedas => new Promise(resolve => resolve(criptomonedas));
 
 document.addEventListener('DOMContentLoaded', () => {
-  obtenerCriptomenedas();
+  consultarCriptomenedas();
 })
 
-function obtenerCriptomenedas() {
+function consultarCriptomenedas() {
   const url = 'https://min-api.cryptocompare.com/data/top/mktcapfull?limit=10&tsym=USD';
 
   fetch(url)
   .then((response) => response.json())
-  .then(results => console.log(results))
+  .then(result => obtenerCriptomonedas(result.Data))
+  .then(criptomonedas => selectCriptomonedas(criptomonedas));
+}
+
+function selectCriptomonedas(criptomonedas) {
+  criptomonedas.forEach(cripto => {
+    const { FullName, Name } = cripto.CoinInfo;
+
+    const option = document.createElement('OPTION');
+    option.textContent = FullName;
+    option.value = Name;
+
+    criptomenedasSelect.appendChild(option);
+  });
+  console.log(criptomonedas);
 }
